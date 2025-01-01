21834914514b1f43b69db754939d741a81b7d4f1
hichammou <moulilihicham8@gmail.com>
2025-01-01 22:14:49 +0100

    refactor: remove the task operations from main.go

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"flag"
 	"log"
-	"os"
 )
 
 var (
@@ -55,40 +54,4 @@ func validateOperation(op *string) error {
 	}
 
 	return nil
-}
-
-func createTask(content string) error {
-	file, err := openTasks("tasks.txt")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.Write([]byte(content + "\n"))
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func readTasks() (string, error) {
-	content, err := os.ReadFile("tasks.txt")
-	if err != nil {
-		return "", nil
-	}
-	return string(content), nil
-}
-
-func openTasks(name string) (*os.File, error) {
-	_, err := os.Stat(name)
-	if os.IsNotExist(err) {
-		file, err := os.Create(name)
-		if err != nil {
-			return nil, err
-		}
-		return file, nil
-	}
-
-	return os.OpenFile(name, os.O_RDWR|os.O_APPEND, 0)
 }
